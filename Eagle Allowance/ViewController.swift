7e043a8150063f033f0c090104628cf5c338c1f0
Joseph Marasco <marascoj@bc.edu>
2019-11-29 14:06:56 -0500

    Initial Commit

---
+++ Eagle Allowance/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Eagle Allowance
+//
+//  Created by Joseph Marasco on 11/29/19.
+//  Copyright © 2019 Joseph Marasco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
